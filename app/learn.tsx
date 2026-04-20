24f55f60474de77bb01131345b8ff6379b9cd2ee
atikarapiah <atikarapiah432@gmail.com>
2026-04-20 13:05:20 +0800

    feat: new feature page

---
+++ app/learn.tsx
@@ -0,0 +1,23 @@
+export default function learning() {
+    return (
+    <div>
+        <div>
+            <img src="/palette.svg" alt="Design" />
+            <p>Design</p>
+        </div>
+    
+        
+
+
+        <div>
+            <p>Figma</p>
+            <p>8 hours ago</p>
+        </div>
+
+        <div>
+            <p>Adobe photoshop</p>
+            <p>4 hours ago</p>
+        </div>
+        </div>
+    )
+    };

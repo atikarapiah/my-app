9f0ad3bc56df41998ab32764f5e45ed7edd674e6
atikarapiah <atikarapiah432@gmail.com>
2026-04-27 13:06:32 +0800

    update css

---
+++ app/learn/page.tsx
@@ -0,0 +1,43 @@
+export default function learnpage() {
+    return (
+    <section className="h-screen bg-gray-100 flex gap-4">
+    <div className="bg-purple-300 p-6 h-55 w-68 rounded-4xl flex flex-col justify-between text-blue-950">
+        <div className="flex gap-2 font-bold">
+            <img src="/palette.svg" alt="design"/>
+                <p>Design</p>
+        </div>
+        <div>
+            <h2 className="text-3xl font-bold">figma</h2>
+            <p className="font-bold">8 Hours ago</p>
+            </div>
+            </div> 
+
+    <div className="bg-orange-200 p-6 h-55 w-68 rounded-4xl flex flex-col justify-between text-blue-950">
+        <div className="flex gap-2 font-bold">
+            <img src="/palette.svg" alt="design"/>
+                <p>Design</p>
+        </div>
+        <div>
+            <h2 className="text-3xl font-bold">figma</h2>
+            <p className="font-bold">2 Hours ago</p>
+            </div>
+            </div> 
+
+    <div className="bg-red-300 p-6 h-55 w-68 rounded-4xl flex flex-col justify-between text-blue-950">
+        <div className="flex gap-2 font-bold">
+            <img src="/palette.svg" alt="design"/>
+                <p>Design</p>
+        </div>
+        <div>
+            <h2 className="text-3xl font-bold">sketch</h2>
+            <p className="font-bold">4 day ago</p>
+            </div>
+            </div>     
+            
+        constructor(parameters) {
+            
+        };
+    
+    </section>
+    )
+}
